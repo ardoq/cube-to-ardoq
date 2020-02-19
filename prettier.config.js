482792e6fa1649bc659fd767bf1cc6feeafc0373
Anton Abilov <anton_abilov@hotmail.com>
2020-02-19 17:13:54 +0100

    Changed entrypoint from main to index.ts

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    trailingComma: 'es5',
-    singleQuote: true,
-    printWidth: 80,
-    parser: 'typescript',
-  };
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  parser: 'typescript',
+};

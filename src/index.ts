ba6ffa0f70b3f2fcb628ad82aa4905acbbecd8a5
Anton Abilov <anton_abilov@hotmail.com>
2020-03-13 18:56:26 +0100

    Extended Cube parsing to include Dimensions and Measures
     - Dimensions and measures are children of cubes

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,130 @@ import { sync, Graph, ApiProperties } from 'ardoq-sdk-js';
 
 type ArdoqCubeGraph = Graph<{}, {}>;
 
+type CubeMeasure = {
+  sql?: (...args: any) => string;
+  type: string;
+  drillMembers: any;
+};
+type CubeDimension = {
+  sql?: (...args: any) => string;
+  case?: any;
+  type: string;
+};
 type Cube = {
   name: string;
   sql: () => string;
+  measures: Record<string, CubeMeasure>;
+  dimensions: Record<string, CubeDimension>;
 };
+
+type EvaluatedCubes = Record<string, Cube>;
 
 type Edge = {
   from: string;
   to: string;
 };
 
+type Select = any;
+
+const addEntitiesFromVisitedTable = (
+  cubeName: string,
+  visitedTable: any,
+  components: any[],
+  references: any[],
+  visitedTables: Set<string>,
+  referenceType = 'SELECT'
+) => {
+  if (!visitedTable.db) {
+    return;
+  }
+  const tableKey = `${visitedTable.db}.${visitedTable.table}`;
+  references.push({
+    customId: `${cubeName}-${tableKey}`,
+    source: cubeName,
+    target: tableKey,
+    type: referenceType,
+  });
+  if (!visitedTables.has(tableKey)) {
+    visitedTables.add(tableKey);
+    components.push({
+      customId: tableKey,
+      workspace: 'cubes',
+      name: tableKey,
+      type: 'Table',
+      fields: [],
+    });
+  }
+};
+
+const addEntitiesFromSqlAST = (
+  sourceName: string,
+  parsedSql: Select,
+  components: any[],
+  references: any[],
+  visitedTables: Set<string>
+) => {
+  if (!Array.isArray(parsedSql) && parsedSql.type === 'select') {
+    parsedSql.from.forEach(visitedTable => {
+      addEntitiesFromVisitedTable(
+        sourceName,
+        visitedTable,
+        components,
+        references,
+        visitedTables
+      );
+    });
+    if (parsedSql.with) {
+      parsedSql.with.forEach(({ stmt }) => {
+        addEntitiesFromSqlAST(
+          sourceName,
+          stmt.ast,
+          components,
+          references,
+          visitedTables
+        );
+      });
+    }
+    if (parsedSql._next) {
+      addEntitiesFromSqlAST(
+        sourceName,
+        parsedSql._next,
+        components,
+        references,
+        visitedTables
+      );
+    }
+  }
+};
+
+const getMeasureDescription = (measure: CubeMeasure) => {
+  return `
+### Calculation
+    ${measure.sql ? measure.sql.toString() : measure.type}
+`;
+};
+
+const getDimensionDescription = (dimension: CubeDimension) => {
+  return `
+### Calculation
+    ${dimension.sql ? dimension.sql.toString() : ''}
+    ${dimension.case ? JSON.stringify(dimension.case, null, 2) : ''}
+`;
+};
+
+const getCubeId = (cubeName: string) => cubeName;
+const getMeasureId = (cubeName: string, measureKey: string) =>
+  `dimension-${cubeName}->${measureKey}`;
+const getDimensionId = (cubeName: string, dimensionKey: string) =>
+  `dimension-${cubeName}->${dimensionKey}`;
+
 export const getGraphFromCube = (metaTransformer: any): ArdoqCubeGraph => {
   const joinGraph = metaTransformer.joinGraph;
-  const evaluatedCubes = metaTransformer.cubeEvaluator.evaluatedCubes;
+  const evaluatedCubes = metaTransformer.cubeEvaluator
+    .evaluatedCubes as EvaluatedCubes;
   const sqlParser = new Parser();
   const cubeComponents = Object.entries(evaluatedCubes).map(
-    ([cubeKey, cube]: [string, Cube]) => {
+    ([cubeKey, cube]) => {
       return {
         customId: cubeKey,
         workspace: 'cubes',
@@ -28,7 +136,6 @@ export const getGraphFromCube = (metaTransformer: any): ArdoqCubeGraph => {
       };
     }
   );
-  const visitedTables = new Set();
   const references = Object.entries(joinGraph.edges).map(
     ([edgeKey, edge]: [string, Edge]) => ({
       customId: edgeKey,
@@ -37,45 +144,66 @@ export const getGraphFromCube = (metaTransformer: any): ArdoqCubeGraph => {
       type: 'Joins',
     })
   );
-  const tableComponents = Object.values(evaluatedCubes).flatMap(
-    (cube: Cube) => {
-      try {
-        const parsedSql = sqlParser.astify(cube.sql());
-        if (!Array.isArray(parsedSql) && parsedSql.type === 'select') {
-          return parsedSql.from
-            .map(visitedTable => {
-              // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-              // @ts-ignore
-              const tableKey = `${visitedTable.db}.${visitedTable.table}`;
-              references.push({
-                customId: `${cube.name}-${tableKey}`,
-                source: cube.name,
-                target: tableKey,
-                type: 'SELECT',
-              });
-              if (!visitedTables.has(tableKey)) {
-                visitedTables.add(tableKey);
-                return {
-                  customId: tableKey,
-                  workspace: 'cubes',
-                  name: tableKey,
-                  type: 'Table',
-                  fields: [],
-                };
-              }
-            })
-            .filter(Boolean);
+  const dimensionComponents = Object.entries(evaluatedCubes).flatMap(
+    ([, cube]: [string, Cube]) => {
+      return Object.entries(cube.dimensions).map(
+        ([dimensionKey, dimension]) => {
+          const customId = getDimensionId(cube.name, dimensionKey);
+          return {
+            customId,
+            workspace: 'cubes',
+            parent: getCubeId(cube.name),
+            name: dimensionKey,
+            fields: [],
+            type: 'Dimension',
+            description: getDimensionDescription(dimension),
+          };
         }
-      } catch (e) {
-        console.log(`ERROR WHEN PARSING ${cube.name}`, cube);
-      }
-      return [];
+      );
     }
   );
-  return {
-    components: [...cubeComponents, ...tableComponents],
+  const measureComponents = Object.entries(evaluatedCubes).flatMap(
+    ([, cube]: [string, Cube]) => {
+      return Object.entries(cube.measures).map(([measureKey, measure]) => {
+        const customId = getMeasureId(cube.name, measureKey);
+        return {
+          customId,
+          workspace: 'cubes',
+          parent: getCubeId(cube.name),
+          name: measureKey,
+          fields: [],
+          type: 'Measure',
+          description: getMeasureDescription(measure),
+        };
+      });
+    }
+  );
+  const visitedTables = new Set<string>();
+  const tableComponents = [];
+  Object.values(evaluatedCubes).forEach(cube => {
+    try {
+      const parsedSql = sqlParser.astify(cube.sql());
+      addEntitiesFromSqlAST(
+        cube.name,
+        parsedSql,
+        tableComponents,
+        references,
+        visitedTables
+      );
+    } catch (e) {
+      console.log(`ERROR WHEN PARSING ${cube.name}`, e.message);
+    }
+  });
+  const graph = {
+    components: [
+      ...cubeComponents,
+      ...dimensionComponents,
+      ...measureComponents,
+      ...tableComponents,
+    ],
     references,
   };
+  return graph;
 };
 
 export const syncCubeGraphToArdoq = (
